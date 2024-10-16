57d1fe2c4e33f036af342c694e61485fd138699a
Emily-OBrien-NHS <e.obrien6@nhs.net>
2024-10-16 11:15:49 +0100

    Updated filename to meet with naming conventions

--- BI_run.py
+++ BI_run.py
@@ -1,6 +1,6 @@
 import importlib.util
-spec = importlib.util.spec_from_file_location("Anchor Strategy.anchor_bi",
-                                              'G:/PerfInfo/Performance Management/OR Team/BI Reports/Anchor Strategy/anchor_bi.py')
+spec = importlib.util.spec_from_file_location("Anchor Strategy.PY_PBI0021_FACT_AnchorStrategy",
+                                              'G:/PerfInfo/Performance Management/OR Team/BI Reports/Anchor Strategy/PY_PBI0021_FACT_AnchorStrategy.py')
 module = importlib.util.module_from_spec(spec)
 import pandas as pd
 spec.loader.exec_module(module)
